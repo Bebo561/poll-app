433e1dcab4610f0d71bf514a751512045a446015
Bebo561 <soto.carlodavid@gmail.com>
2023-03-06 17:08:05 -0500

    Added basic webpage and css, no functionality

---
+++ src/project files/login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./login.css";
+
+const loginPage = () => {
+    return (
+
+        <div id = "Form">
+            <h1>Login Form</h1>
+            <input type = "text" placeholder = "Username" />
+            <input type = "password" placeholder = "Password"/>
+
+            <button id="signIn">Sign In</button>
+
+            <p>Don't Have An Account?</p>
+            <br></br>
+            <span className = "line">
+                <a href ="#">User Register</a>
+            </span>
+        </div>
+    )
+}
+
+export default loginPage
