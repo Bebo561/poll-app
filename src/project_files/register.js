d1421d9442bea4eb3c0ff52086e33aa93494828c
Bebo561 <soto.carlodavid@gmail.com>
2023-03-07 12:26:58 -0500

    Edited frontend aspects

--- src/project_files/register.js
+++ src/project_files/register.js
@@ -6,12 +6,15 @@ const Register = () => {
         <React.Fragment>
        <div id ="Half1">
         <h1>Register Here</h1>
+        <h3>Username</h3>
         <input type = "text" id="user" placeholder = "Username" />
+        <h3>Password</h3>
         <input type = "password" id="pass" placeholder = "Password"/>
         <button id="signUp">Sign Up!</button>
        </div>
        <div id ="Half2">
-        
+            <h1>Welcome to PollsrUS</h1>
+            <h4>Where your vote matters, Sign Up Today</h4>
        </div>
        </React.Fragment>
     )
