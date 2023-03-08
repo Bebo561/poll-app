0d138d431a6bc91595f312140abdf166f8174696
Bebo561 <soto.carlodavid@gmail.com>
2023-03-08 15:52:07 -0500

    Frontend functions to backend request for login page added

--- src/project_files/login.js
+++ src/project_files/login.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import "./login.css";
 import { Link} from 'react-router-dom';
+import axios from 'axios';
 
-const Login = () => {
-    return (
+class Login extends React.Component{
+    constructor(props){
+        super(props);
 
-        <form id = "Form" method="POST" action = "/login"> 
-            <h1>Login Form</h1>
-            <input type = "text" id = "Log"placeholder = "Username" />
-            <input type = "password" id = "Log"placeholder = "Password"/>
+        this.state = {
+            username: '',
+            password: ''
+        };
+    } 
+    handleSub = (event) =>{
+        event.preventDefault();
+        const url = 'http://localhost:3001/login';
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        };
+        console.log(user);
+        axios.post(url, user).then((res) => {
+            console.log(res);
+            alert("Successful Submission!");
+            this.props.navigate('/')
+        }).catch((error) => {
+            alert(error.message);
+        })
+    }
+    handleInputChnage= (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    render(){
+        return (
+            <React.Fragment>
+                <form id = "Form" method="POST" action = "/login"> 
+                    <h1>Login Form</h1>
+                    <input type = "text" id = "Log" name ="username" onChange={this.handleInputChnage} placeholder = "Username" />
+                    <input type = "password" id = "Log" name ="password" onChange={this.handleInputChnage} placeholder = "Password"/>
 
-            <button id="signIn">Sign In</button>
-            <Link to ="/register" id="Reg">Don't Have An Account?</Link>
-                
-        </form>
+                    <button id="signIn" onClick={this.handleSub}>Sign In</button>
+                    <Link to ="/register" id="Reg">Don't Have An Account?</Link>
+                </form>
+             </React.Fragment>
     )
+    }
 }
 
 export default Login
