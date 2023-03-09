54e593996fa5c254d037bed5f415e5e6e08c30f2
Bebo561 <soto.carlodavid@gmail.com>
2023-03-09 13:47:24 -0500

    Created general outline of poll creation form, no CSS yet

--- src/project_files/createVote.js
+++ src/project_files/createVote.js
@@ -1,13 +1,40 @@
 import React from "react"
 import './createVote.css'
+import {Link} from 'react-router-dom';
+import {withRouter} from './withRouter';
+import axios from 'axios';
 
 class createVote extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            pollName: '',
+            Option1: '',
+            numOfVotes1: '',
+            Option2: '',
+            numOfVotes2: '',
+            Option3: '',
+            numOfVotes3: ''
+        };
+    } 
+    handlePollInput= (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    handleCreatePoll = (event) =>{
+
+    }
     render() {
     return(
         <React.Fragment>
-            <div id = "Homepage">
-                <h1>Hello World!</h1>   
-            </div>
+            <form id ="pollCreation" method="POST" action="/createVote">
+            <input type = "text" id = "pollName" name="pollName" onChange={this.handlePollInput} placeholder = "Poll Name" />
+            <input type = "text" id = "Option" name="Option1" onChange={this.handlePollInput} placeholder = "Option 1" />
+            <input type = "text" id = "Option" name="Option2" onChange={this.handlePollInput} placeholder = "Option 2" />
+            <input type = "text" id = "Option" name="Option3" onChange={this.handlePollInput} placeholder = "Option 3" />
+            <button id="createPoll" onClick={this.handlePollInput}>Create Poll</button>
+            </form>
         </React.Fragment>
         )
     }
